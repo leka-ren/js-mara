e7a4b3593db38d842a16d8058ce54be780e06a75
leka-ren <polafort@gmail.com>
2020-09-24 15:21:12 +0300

    импорт файлов и зачистка мусора, корректировка стилей

--- main.js
+++ main.js
@@ -1,58 +1,9 @@
-const title = document.querySelector("h1");
-const resoult = document.querySelector("h2");
+buttonRow.addEventListener("click", rowTask);
 
-resoult.style.display = "none";
+document
+  .querySelectorAll("#restart")
+  .forEach((el) =>
+    el.addEventListener("click", () => window.location.reload())
+  );
 
-function showResoult(data) {
-  if (data.amount) {
-    resoult.textContent = `Больше всего символов "${data.char}" в строке "${data.row}", кол-во символов: ${data.amount}`;
-  } else {
-    resoult.textContent = `Во фразах "${data.row}" нет буквы "${data.char}"`;
-  }
-  resoult.style.display = "flex";
-  title.style.display = "none";
-}
-
-function findCharAmount(firstRow, secondRow, char) {
-  let firstCount = 0;
-  let secondCount = 0;
-  let dataRes = {};
-  for (let i = 0; i < firstRow.length; i++) { //вот цикл for :)
-    if (firstRow[i] === char) {
-      firstCount++;
-    }
-    if (secondRow[i] === char) {
-      secondCount++;
-    }
-  }
-  if (firstCount === 0 && secondCount === 0) {
-    dataRes = {
-      row: `${firstRow} и ${secondRow}`,
-      char,
-    };
-  } else {
-    dataRes = {
-      row: firstCount > secondCount ? firstRow : secondRow,
-      amount: firstCount > secondCount ? firstCount : secondCount,
-      char,
-    };
-  }
-  showResoult(dataRes);
-}
-
-function takePhrase() {
-  const firstRow = prompt("введите первую фразу:");
-  const secondRow = prompt("введите вторую фразу:");
-  const WhatCharFound = prompt("введите букву которую искать:");
-
-  findCharAmount(firstRow, secondRow, WhatCharFound);
-}
-
-function resetContent() {
-  resoult.textContent = "";
-  resoult.style.display = "none";
-  title.style.display = "flex";
-}
-
-title.addEventListener("click", takePhrase);
-resoult.addEventListener("click", resetContent);
+buttonNum.addEventListener("click", numTask);
